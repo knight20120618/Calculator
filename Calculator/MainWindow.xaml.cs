f348e91fde67dbf8c88115a18952928df1a16578
knight20120618 <125259069+knight20120618@users.noreply.github.com>
2023-04-17 15:59:44 +0800

    程式碼大致設定
    剩一些還未完成

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calculator
 {
@@ -25,6 +13,20 @@ namespace Calculator
             InitializeComponent();
         }
 
+        //firstNumber儲存第一個數字 | secondNumber儲存第二個數字
+        float firstNumber, secondNumber;
+        int operators = -1;
+
+        private void btnZero_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNumber.Text == "0")
+            {
+                txtNumber.Text = "";
+            }
+
+            txtNumber.Text += "0";
+        }
+
         private void btnOne_Click(object sender, RoutedEventArgs e)
         {
             if (txtNumber.Text == "0")
@@ -32,7 +34,7 @@ namespace Calculator
                 txtNumber.Text = "";
             }
 
-            txtNumber.Text = txtNumber.Text + "1";
+            txtNumber.Text += "1";
         }
 
         private void btnTwo_Click(object sender, RoutedEventArgs e)
@@ -42,7 +44,7 @@ namespace Calculator
                 txtNumber.Text = "";
             }
 
-            txtNumber.Text = txtNumber.Text + "2";
+            txtNumber.Text += "2";
         }
 
         private void btnThree_Click(object sender, RoutedEventArgs e)
@@ -52,7 +54,7 @@ namespace Calculator
                 txtNumber.Text = "";
             }
 
-            txtNumber.Text = txtNumber.Text + "3";
+            txtNumber.Text += "3";
         }
 
         private void btnFour_Click(object sender, RoutedEventArgs e)
@@ -62,7 +64,7 @@ namespace Calculator
                 txtNumber.Text = "";
             }
 
-            txtNumber.Text = txtNumber.Text + "4";
+            txtNumber.Text += "4";
         }
 
         private void btnFive_Click(object sender, RoutedEventArgs e)
@@ -72,7 +74,7 @@ namespace Calculator
                 txtNumber.Text = "";
             }
 
-            txtNumber.Text = txtNumber.Text + "5";
+            txtNumber.Text += "5";
         }
 
         private void btnSix_Click(object sender, RoutedEventArgs e)
@@ -82,7 +84,7 @@ namespace Calculator
                 txtNumber.Text = "";
             }
 
-            txtNumber.Text = txtNumber.Text + "6";
+            txtNumber.Text += "6";
         }
 
         private void btnSeven_Click(object sender, RoutedEventArgs e)
@@ -92,7 +94,7 @@ namespace Calculator
                 txtNumber.Text = "";
             }
 
-            txtNumber.Text = txtNumber.Text + "7";
+            txtNumber.Text += "7";
         }
 
         private void btnEight_Click(object sender, RoutedEventArgs e)
@@ -102,7 +104,7 @@ namespace Calculator
                 txtNumber.Text = "";
             }
 
-            txtNumber.Text = txtNumber.Text + "8";
+            txtNumber.Text += "8";
         }
 
         private void btnNine_Click(object sender, RoutedEventArgs e)
@@ -112,17 +114,68 @@ namespace Calculator
                 txtNumber.Text = "";
             }
 
-            txtNumber.Text = txtNumber.Text + "9";
+            txtNumber.Text += "9";
         }
 
-        private void btnZero_Click(object sender, RoutedEventArgs e)
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            if (txtNumber.Text == "0")
+            firstNumber = Convert.ToSingle(txtNumber.Text); //將輸入文字框轉換成浮點數，存入第一個數字的全域變數
+            txtNumber.Text = "+";                           //重新將輸入文字框重新設定為0
+            operators = 0;                                  //選擇「加」號
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "-";
+            operators = 1;                   //選擇「減」號
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "*";
+            operators = 2;                   //選擇「乘」號
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "/";
+            operators = 3;                   //選擇「除」號
+        }
+
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            //宣告最後計算結果變數
+            float finalResults = 0f;
+            //將輸入文字框轉換成浮點數，存入第二個數字的全域變數
+            secondNumber = Convert.ToSingle(txtNumber.Text);
+
+            //依照四則運算符號的選擇，進行加減乘除
+            switch (operators)
             {
-                txtNumber.Text = "";
+                case 0:
+                    finalResults = firstNumber + secondNumber;
+                    break;
+                case 1:
+                    finalResults = firstNumber - secondNumber;
+                    break;
+                case 2:
+                    finalResults = firstNumber * secondNumber;
+                    break;
+                case 3:
+                    finalResults = firstNumber / secondNumber;
+                    break;
             }
 
-            txtNumber.Text = txtNumber.Text + "0";
+            //在輸入文字框中，顯示最後計算結果，並且轉換成格式化的字串內容
+            txtNumber.Text = string.Format("{0:0.##########}", finalResults);
+
+            //重置所有全域變數
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
         }
     }
 }
